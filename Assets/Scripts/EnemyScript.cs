2313a758883e77bf6c7ffcb6f9d0ef76b8780830
Faiza-Maghrabi <faiza.maghrabi@gmail.com>
2024-10-15 17:04:18 +0100

    feat: enemy spotting player, tags, player layer

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public float fovAngle;
+    public float detectionRadius;
     private Transform player;
-
     // Start is called before the first frame update
 
     void Start()
@@ -14,20 +18,33 @@ public class EnemyScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        RaycastHit hit;
-        var rayDirection = player.position - transform.position;
+        bool playerInView = false;
 
-        if (Physics.Raycast(transform.position, rayDirection, 10)) {
+        playerInView = FoundPlayer();
+        
 
-            if (hit.transform == player) {
-                // enemy can see the player!
-            } else {
-                // there is something obstructing the view
+    }
+
+    private bool FoundPlayer() {
+        int layerMask = LayerMask.GetMask("PlayerLayer");
+        Collider[] FOVTargets = Physics.OverlapSphere(transform.position, detectionRadius, layerMask);
+        if (FOVTargets.Count() > 0) {
+            Vector3 directionToPlayer = (FOVTargets[0].transform.position - transform.position).normalized;
+            float angle = Vector3.Angle(transform.forward, directionToPlayer);
+
+            RaycastHit hit;
+            if (angle < fovAngle / 2) {
+                if (Physics.Raycast(transform.position, directionToPlayer, out hit, detectionRadius)) {
+                    if (hit.transform == player) {
+                        Debug.Log("player spotted");
+                        return true;
+                    }
+                }
             }
-
         }
-
+        Debug.Log("not found");
+        return false;
     }
 }
