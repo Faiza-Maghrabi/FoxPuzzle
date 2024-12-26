7a8c299b7b91db770eb35380a0cdca42aa4d1880
Faiza-Maghrabi <faiza.maghrabi@gmail.com>
2024-12-26 21:28:43 +0000

    chore: test updated version
    scene coming up as null - am i parsing this wrong?

--- Assets/Scripts/FoodTracker.cs
+++ Assets/Scripts/FoodTracker.cs
@@ -4,37 +4,43 @@ using UnityEngine;
 using System.IO;
 using System;
 using Unity.VisualScripting;
+using Mono.Collections.Generic;
 
 //will load up a json of food in each secene and assign to a static list
 //this list is referred to by players on contact and keeps track of food that has been collected
 //on food load, the object checks here if it should exist - if it should not then it is destroyed.
 
-[System.Serializable]
-//Class to hold data each JSON object obtains
-public class SceneFood
-{
-    public string name;
-    public bool active;
-}
+// [System.Serializable]
+// //Class to hold data each JSON object obtains
+// public class SceneFood
+// {
+//     public string name;
+//     public bool active;
+// }
+
+// [System.Serializable]
+// //Class to hold data each JSON object obtains
+// public class SceneItem
+// {
+//     public string name;
+//     public List<SceneFood> food;
+// }
+
+// [System.Serializable]
+// //Class to create a disctionary of scenes with their foodlists
+// public class SceneFoodList {
+//     public List<SceneItem> scene;
+// }
 
 [System.Serializable]
-//Class to hold data each JSON object obtains
-public class SceneItem
-{
-    public string name;
-    public List<SceneFood> food;
+public class testDict {
+    public Dictionary<string, Dictionary<string, bool>> scene;
 }
-
-[System.Serializable]
-//Class to create a disctionary of scenes with their foodlists
-public class SceneFoodList {
-    public List<SceneItem> scene;
-}
-
 
 public class FoodTracker : MonoBehaviour
 {
-    private static SceneFoodList sceneFoodList;
+    //private static SceneFoodList sceneFoodList;
+    private static testDict test;
     private static string jsonFilePath;
 
     public static void Init() {
@@ -43,27 +49,21 @@ public class FoodTracker : MonoBehaviour
         if (File.Exists(jsonFilePath)){
             string jsonContent = File.ReadAllText(jsonFilePath);
             Debug.Log(jsonContent);
-            sceneFoodList = JsonUtility.FromJson<SceneFoodList>(jsonContent);
-            //List<string> idk;
-            Debug.Log(sceneFoodList);
-
-            // listJson.TryGetValue("OpenHouseScene", out idk);
-            Debug.Log(sceneFoodList.scene[0].name);
-            Debug.Log(sceneFoodList.scene[0].food[0].name);
-    
-            // foreach (KeyValuePair<string, List<string>> kvp in listJson.scene)
-            // {
-            //     //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            //     Debug.Log(string.Format("Key = {0}, Value = {1}", kvp.Key, kvp.Value));
-            // }
-            //Debug.Log(listJson.scene.["OpenHouseScene"][0]);
+            test = JsonUtility.FromJson<testDict>(jsonContent);
+            Debug.Log(test);
+            Debug.Log(test.scene);
+            Debug.Log(test.scene["OpenHouseScene"]);
+            // sceneFoodList = JsonUtility.FromJson<SceneFoodList>(jsonContent);
+            // Debug.Log(sceneFoodList);
+            // Debug.Log(sceneFoodList.scene[0].name);
+            // Debug.Log(sceneFoodList.scene[0].food[0].name);
         }
         else {
             Debug.LogError("No file at " + jsonFilePath);
         }
     }
 
-    // public bool isCollected(string scene, string name) {
+    // public bool isCollected(string sceneIndex, string foodName) {
     //     if (sceneFoodList.[scene].name)
     // }
 }
