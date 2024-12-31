00d5f9c11bccb8efba1496c9bb18753ad33874d7
nbal21388 <nadiabalbontin@hotmail.com>
2024-12-31 07:51:38 -0600

    MazeGenerator Script

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private MazeCell _mazeCellPrefab;
+
+    private int _mazeWidth;
+
+    private int _mazeDepth;
+
+    private MazeCell[,] _mazeGrid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mazeGrid = new MazeCell[_mazeWidth, _mazeDepth];
+
+        for (int x = 0; x < _mazeWidth; x++)
+        {
+            for (int z = 0; z < _mazeDepth; z++)
+            {
+                _mazeGrid[x, z] = Instantiate(_mazeCellPrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
