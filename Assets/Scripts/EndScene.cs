e88059cd7f8a6f7a53cb2c55be3bef2a936cf8f1
quytra25 <98822595+quytra25@users.noreply.github.com>
2025-01-03 18:24:39 +0000

    Update EndScene.cs - adding conditional game message

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -7,6 +7,7 @@ public class EndScene : MonoBehaviour
 {
     //private PlayerController player;
     public TMP_Text scoreText;
+    public TMP_Text messageText;
 
     void Awake(){
         Time.timeScale = 0;
@@ -22,6 +23,28 @@ public class EndScene : MonoBehaviour
         Time.timeScale = 0;
         Cursor.lockState = CursorLockMode.None; 
         Cursor.visible = true;
-        scoreText.text = "You have accumilated a score of : " + PlayerController.score.ToString();    
+        scoreText.text = "You have accumilated a score of : " + PlayerController.score.ToString();
+        
+        if (PlayerController.score >= 3500)
+        {
+            messageText.text = "Absolute top tier player. Your reward is a round of applause from the developers of the game.";
+        }
+        else if (PlayerController.score >= 2500)
+        {
+            messageText.text = "Amazing high score! Well Done :)";
+        }
+        else if (PlayerController.score >= 1500)
+        {
+            messageText.text = "Solid score. Good job and keep it up!";
+        }
+        else if (PlayerController.score >= 500)
+        {
+            messageText.text = "You gave it your all, and that’s what counts!";
+        }
+        else
+        {
+            messageText.text = "Was that a warm-up? Let’s see your real skills next time!";
+        }
+
     }
 }
